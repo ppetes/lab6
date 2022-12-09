ed25d0a13f0c26677d8a9cad5e25f98afbb294fe
Ratchapon Prangthong <ppete2547@gmail.com>
2022-12-09 15:21:42 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,20 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+int N;
+int i = 0;
+int x = 0;
+while(N != 0){
+    cout << "Enter an integer: " ; 
+    cin >> N;
+    if(N%2 == 0){
+    i++;}
+    else{
+    x++;
+}
+}
+    cout << "#Even numbers = " << i-1 << endl;
+    cout << "#Odd numbers = " << x << endl;
     return 0;
+
 }
